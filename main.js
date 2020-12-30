928e7fcc60fa044020ede6771704a2e10527f742
shawn220a <shawn.42496@gmail.com>
2020-12-30 16:06:54 -0600

    Create event listener to read barcode value

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+const barcode = document.querySelector('#barcodeInput')
+const generateBarcode = document.querySelector('#generateBarcode')
+
+generateBarcode.addEventListener('click', function (e) {
+  e.preventDefault()
+  console.log('hello')
+});
